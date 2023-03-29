41d95e47354566e7260e4c6e375deff0ebb281e5
Brenden Olding <oldingb21@gmail.com>
2023-03-29 19:46:53 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+const currentUser = "Brenden Olding";
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+        //can also make this string work by writing, `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
